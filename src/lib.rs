46d12ce3fd5765ed48459d527c6d52c4bdb2562c
Alexandru Fikl <alexfikl@gmail.com>
2023-04-15 21:16:14 +0300

    add some rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 use pyo3::prelude::*;
+use num::complex::Complex;
+
+use ml_series::ml_series;
+
+mod ml_series;
 
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn ml(z: f64, alpha: f64, beta: f64) -> PyResult<usize> {
+    let _r = ml_series(Complex::new(z, z), alpha, beta);
+    Ok(0)
+}
+
 #[pymodule]
 fn _mittagleffler(_py: Python, m: &PyModule) -> PyResult<()> {
+    ml(0.0, 0.0, 0.0)?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
---
+++ src/ml_series.rs
@@ -0,0 +1,5 @@
+use num::complex::Complex;
+
+pub fn ml_series<T>(z: Complex<T>, _alpha: T, _beta: T) -> Complex<T> {
+    z
+}
