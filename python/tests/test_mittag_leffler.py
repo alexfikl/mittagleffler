b0b7f732d3aba736e773c23c573aeb3235c0484b
Alexandru Fikl <alexfikl@gmail.com>
2025-12-30 11:49:08 +0200

    chore: remove unused comment

--- python/tests/test_mittag_leffler.py
+++ python/tests/test_mittag_leffler.py
@@ -29,7 +29,7 @@ def test_mittag_leffler_scalar() -> None:
 
     z = "1.0"
     with pytest.raises(TypeError):
-        _ = mittag_leffler(z, alpha, beta)  # type: ignore[call-overload]
+        _ = mittag_leffler(z, alpha, beta)
 
 
 @pytest.mark.parametrize(
